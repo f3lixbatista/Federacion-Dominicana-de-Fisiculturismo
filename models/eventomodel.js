6658236c81ab5a2b75d8ed662b42f713dacb205d
f3lixbatista <felixdni@hotmail.com>
2022-11-29 13:55:01 -0400

    Create eventomodel.js

---
+++ models/eventomodel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const categoriaSchema = new Schema({
+    Categoria: String,
+    Modalidad: String,
+    Genero: String,
+    Disciplina: String,
+    Division: String,
+    DesdeEdad: Number,
+    HastaEdad: Number,
+    DesdePeso: Number,
+    HastaPeso: Number,
+    DesdeEstatura: Number,
+    HastaEstatura: Number,
+   
+})
+
+
+// crear modelo
+const Categoria = mongoose.model('Categoria', categoriaSchema);
+
+
+module.exports = Categoria;
